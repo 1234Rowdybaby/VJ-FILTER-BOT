65731c3fed5fbea2efa5e910c4a88e5267028cbd
1234Rowdybaby <167402968+1234Rowdybaby@users.noreply.github.com>
2024-12-26 15:09:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/4f4e681b77b55943cc2d7-b4e02ae73c03911b7e.jpg')).split()
 
 
 # Admins & Users
